d148ea2df5f68fa0844ab90a95de7fdd5808614d
lee <1079811936@qq.com>
2021-07-23 20:26:14 +0800

    当前路由图标自动高亮

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import Icon from './Icon'
 
@@ -20,6 +20,12 @@ const NavWrapper = styled.nav`
         width:24px;
         height:24px;
         }
+        &.selected{
+          color:red;
+          .icon{
+            fill:red;
+          }
+        }
       }
     }
   }
@@ -30,22 +36,22 @@ const Nav = () => {
         <NavWrapper>
           <ul>
             <li>
-              <Link to="/tags">
+              <NavLink to="/tags" activeClassName="selected">
                 <Icon name='tag' />
                 标签
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link to="/">
+              <NavLink to="/money" activeClassName="selected">
                 <Icon name='money' />
                 记账
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link to="/statistics">
+              <NavLink to="/statistics" activeClassName="selected">
               <Icon name='chart' />
                 统计
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </NavWrapper>
