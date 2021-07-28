a1a96ba2fc00570b3bd668b9ae9797db9aa67dff
lee <1079811936@qq.com>
2021-07-28 18:34:01 +0800

    点击回退

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -21,16 +21,10 @@ const useTags = () => {
         return result
     }
     const updateTag = (id:number,obj:{name:string}) => {
-        const index = findTagIndex(id)
-        const tagsClone = JSON.parse(JSON.stringify(tags))
-        tagsClone.splice(index,1,{id:id,name:obj.name})
-        setTags(tagsClone)
+        setTags(tags.map(tag => tag.id === id ? {id,name:obj.name}:tag))
     } 
     const deleteTag = (id:number) => {
-        const index = findTagIndex(id)
-        const tagsClone = JSON.parse(JSON.stringify(tags))
-        tagsClone.splice(index,1)
-        setTags(tagsClone)
+        setTags(tags.filter(tag=>tag.id !== id))
     }
 
     return {
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -4,9 +4,10 @@ import Icon from 'components/Icon';
 import Layout from 'components/Layout';
 import { Space } from 'components/Space';
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams,useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { useTags } from 'useTags';
+
 type Params = {
     id:string
 }
@@ -58,10 +59,14 @@ const Tag:React.FC = () => {
                 </Center>
         </div>
     )
+    const history = useHistory()
+    const onClickBack = () => {
+       history.goBack()
+    }
     return (
         <Layout>
             <Topbar>
-                <Icon name="left" />
+                <Icon name="left" onClick={onClickBack}/>
                 <span>编辑标签</span>
                 <Icon/>
             </Topbar>
