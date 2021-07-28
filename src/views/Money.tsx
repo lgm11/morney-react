2eefd3e1b3ea5ee0b92978789db5ef990efad373
lee <1079811936@qq.com>
2021-07-29 06:39:39 +0800

    检查输入

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -31,9 +31,12 @@ function Money() {
   }
   const {addRecord} = useRecords()
   const submit = () => {
-    addRecord(selected)
-    alert('保存成功')
-    setSelected(defaultFormData)
+    if(addRecord(selected)){
+      alert('保存成功')
+      setSelected(defaultFormData)
+    }
+    
+    
   }
     return (
       <MyLayout>
